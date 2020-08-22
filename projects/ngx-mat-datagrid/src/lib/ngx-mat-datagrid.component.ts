aa4bb97bdeb661712dc440117cd68eace3403e2c
johannesalt <47456469+johannesalt@users.noreply.github.com>
2020-08-22 12:25:45 +0200

    angular library 'ngx-mat-datagrid' created

---
+++ projects/ngx-mat-datagrid/src/lib/ngx-mat-datagrid.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-mat-ngx-mat-datagrid',
+  template: `
+    <p>
+      ngx-mat-datagrid works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxMatDatagridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ngx-mat-datagrid/src/lib/ngx-mat-datagrid.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxMatDatagridComponent } from './ngx-mat-datagrid.component';
+
+
+
+@NgModule({
+  declarations: [NgxMatDatagridComponent],
+  imports: [
+  ],
+  exports: [NgxMatDatagridComponent]
+})
+export class NgxMatDatagridModule { }
