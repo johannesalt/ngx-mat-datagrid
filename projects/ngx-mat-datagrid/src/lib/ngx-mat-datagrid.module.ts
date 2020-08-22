3731a39a5936c660775b996e6f4455117328c731
Johannes Alt <altjohannes510@gmail.com>
2020-08-22 16:45:05 +0200

    Create a component which can be used to pin or unpin a row or a column.

--- projects/ngx-mat-datagrid/src/lib/ngx-mat-datagrid.module.ts
+++ projects/ngx-mat-datagrid/src/lib/ngx-mat-datagrid.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { NgxMatDatagridComponent } from './ngx-mat-datagrid.component';
-
+import {NgModule} from '@angular/core';
+import {NgxMatDatagridComponent} from './ngx-mat-datagrid.component';
+import {PinningComponent} from './pinning/pinning.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [NgxMatDatagridComponent],
+  declarations: [
+    NgxMatDatagridComponent,
+    PinningComponent
+  ],
   imports: [
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [NgxMatDatagridComponent]
 })
-export class NgxMatDatagridModule { }
+export class NgxMatDatagridModule {
+}
---
+++ projects/ngx-mat-datagrid/src/lib/pinning/pinning.component.spec.ts
@@ -0,0 +1,54 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {PinningComponent} from './pinning.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {from} from 'rxjs';
+
+describe('PinningComponent', () => {
+  let component: PinningComponent;
+  let fixture: ComponentFixture<PinningComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PinningComponent],
+      imports: [NoopAnimationsModule],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PinningComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should be pinned', () => {
+    component.pinned = false;
+    component.pinOrUnpinRowOrColumn();
+    expect(component.pinned).toBeTrue();
+  });
+
+  it('should emit the pin event', (done) => {
+    component.pinned = false;
+    from(component.pin).subscribe(() => done());
+    component.pinOrUnpinRowOrColumn();
+  })
+
+  it('should be unpinned', () => {
+    component.pinned = true;
+    component.pinOrUnpinRowOrColumn();
+    expect(component.pinned).toBeFalse();
+  });
+
+  it('should emit the unpin event', (done) => {
+    component.pinned = true;
+    from(component.unpin).subscribe(() => done());
+    component.pinOrUnpinRowOrColumn();
+  })
+});
